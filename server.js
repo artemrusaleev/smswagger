c20c6b629e7115a7b32ef5b8418c44fc8c241136
Artem Rusaleev <artyomrusaleev@gmail.com>
2023-01-30 11:03:30 +0300

    Init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }'
+};
+
+app.use('/basket-api', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Swagger app listening on port ${process.env.PORT || 3000}`)
+})
