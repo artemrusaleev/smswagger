57d4c30eec1894bd7257cec29d710dbac28aa9d5
Artem Rusaleev <artyomrusaleev@gmail.com>
2023-01-30 12:35:39 +0300

    :Fix

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
+app.get("*", (req, res, next) => {
+  next();
+});
 app.use("/api/basket", basket);
 
 app.listen(PORT, () => console.log(`Server is running in posrt ${PORT}`));
