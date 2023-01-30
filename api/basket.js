7fdd8ddbb8227846fff896486972db3a71a6b781
Artem Rusaleev <artyomrusaleev@gmail.com>
2023-01-30 12:38:28 +0300

    :Fix

--- api/basket.js
+++ api/basket.js
@@ -4,6 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
 router.use("/", swaggerUi.serve);
+router.use(express.static(__dirname + "/static/"));
 router.get("/", swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
