24062ff1aa5298b3418ba935a37beac597f21b51
Artem Rusaleev <artyomrusaleev@gmail.com>
2023-01-30 11:48:52 +0300

    Fix

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
-const router = require("express").Router();
+const express = require("express");
+const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
-router.use("/api", swaggerUi.serve);
-router.get("/api", swaggerUi.setup(swaggerDocument));
+const options = {
+  customCss: ".swagger-ui .topbar { display: none }",
+};
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Swagger app listening on port ${process.env.PORT || 3000}`);
+});
