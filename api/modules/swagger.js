ce70abb409c7999cbb65a1cf49cb252cc65a0ea9
Artem Rusaleev <artyomrusaleev@gmail.com>
2023-01-30 12:06:08 +0300

    Remove all module files

--- api/modules/swagger.js
+++ api/modules/swagger.js
@@ -1,8 +1,16 @@
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../../swagger.json");
+const fs = require("fs");
+const styles = fs.readFile(
+  "./node_modules/swagger-ui-dist/index.css",
+  "utf8",
+  (err, data) => {
+    return data;
+  }
+);
 
 const options = {
-  customCss: ".swagger-ui .topbar { display: none }",
+  customCss: styles,
 };
 
 module.exports = swaggerUi.setup(swaggerDocument, options);
