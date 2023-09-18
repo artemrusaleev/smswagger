abf37a4d4b54e076a657289e16a89ce6b2334208
Artem Rusaleev <artyomrusaleev@gmail.com>
2023-09-18 11:13:47 +0300

    WIP: start to add main api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const basket = require("./api/basket");
+const main = require("./api/main");
 const app = express();
 const path = require("path");
 
@@ -10,5 +11,7 @@ app.get("*", (req, res, next) => {
 });
 app.use("/api/basket", basket);
 app.use("/api/basket/", express.static(path.join(__dirname, "static")));
+app.use("/api/main", main);
+app.use("/api/main/", express.static(path.join(__dirname, "static")));
 
 app.listen(PORT, () => console.log(`Server is running in posrt ${PORT}`));
