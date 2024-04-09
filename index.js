951b75543c3e6259cc57190275c794608a621099
Artem Rusaleev <artyomrusaleev@gmail.com>
2024-04-09 13:52:13 +0300

    fix: routes fixes

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const basket = require("./api/basket");
-const main = require("./api/main");
 const app = express();
 const path = require("path");
-
 const PORT = process.env.PORT || 5050;
+const swaggerUi = require("swagger-ui-express");
+const main = require("./main.json");
+const basket = require("./basket.json");
 
 app.get("*", (req, res, next) => {
   next();
 });
-app.use("/api/basket", basket);
-app.use("/api/basket/", express.static(path.join(__dirname, "static")));
-app.use("/api/main", main);
-app.use("/api/main/", express.static(path.join(__dirname, "static")));
+app.use("/basket", swaggerUi.serveFiles(basket), swaggerUi.setup(basket));
+app.use("/", swaggerUi.serveFiles(main), swaggerUi.setup(main));
 
 app.listen(PORT, () => console.log(`Server is running in posrt ${PORT}`));
